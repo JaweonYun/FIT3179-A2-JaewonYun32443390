bdd2f420980868b490ad60caca2785116c9b5063
JaweonYun <89090618+JaweonYun@users.noreply.github.com>
2024-10-16 13:30:19 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,20 @@
+var vg_1 = "australia_immigration.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "vega_map.vg.json";
+vegaEmbed("#world_map", vg_2).then(function (result) {
+}).catch(console.error);
+
+var vg_3 = "Top_5_Countries.vg.json";
+vegaEmbed("#Top_5", vg_3).then(function (result) {
+}).catch(console.error);
+
+var vg_4 = "1980_PC.vg.json";
+vegaEmbed("#Pie_Chart_1", vg_4).then(function (result) {
+}).catch(console.error);
+
+var vg_5 = "2018_PC.vg.json";
+vegaEmbed("#Pie_Chart_2", vg_5).then(function (result) {
+}).catch(console.error);
+
